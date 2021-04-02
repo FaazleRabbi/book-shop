750ea700de6b94ec4b82012a458230f6f209972f
FazleRabbi <24.fazlerabbi@gmail.com.com>
2021-04-02 07:01:46 -0600

    add readme file

--- src/Components/AddBook/AddBook.js
+++ src/Components/AddBook/AddBook.js
@@ -1,54 +1,84 @@
+import { LinearProgress } from "@material-ui/core";
 import axios from "axios";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 const AddBook = () => {
-  const [bookImageUrl,setBookImageUrl]=useState(null)
+  const [Linear, setLinear] = useState(false);
+
+  const [bookImageUrl, setBookImageUrl] = useState(null);
 
   const { register, handleSubmit, watch, errors } = useForm();
   const onSubmit = (data) => {
-      const bookInfo ={
-          bookName: data.bookName,
-          authorName:data.authorName,
-          imageUrl:bookImageUrl,
-          price:data.bookPrice
-      }
+    const bookInfo = {
+      bookName: data.bookName,
+      authorName: data.authorName,
+      imageUrl: bookImageUrl,
+      price: data.bookPrice,
+    };
 
-      fetch('https://pacific-earth-93267.herokuapp.com/addBook',{
-          method:"POST",
-          headers:{'content-type':'application/json'},
-          body:JSON.stringify(bookInfo)
-      })
+    fetch("https://pacific-earth-93267.herokuapp.com/addBook", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(bookInfo),
+    });
   };
 
-  const handleBookImage = (e)=>{
-      const bookImage = new FormData();
-      bookImage.set('key','d9df22459998db9719f423a928f646a7')
-      bookImage.append('image',e.target.files[0])
-      console.log(e.target.files[0])
-      console.log(bookImage)
-      axios.post('https://api.imgbb.com/1/upload',bookImage)
+  const handleBookImage = (e) => {
+    setLinear(true);
+    const bookImage = new FormData();
+    bookImage.set("key", "d9df22459998db9719f423a928f646a7");
+    bookImage.append("image", e.target.files[0]);
+    console.log(e.target.files[0]);
+    console.log(bookImage);
+    axios
+      .post("https://api.imgbb.com/1/upload", bookImage)
       .then(function (response) {
         setBookImageUrl(response.data.data.display_url);
-        console.log(response.data.data.display_url)
+        setLinear(false);
+        console.log(response.data.data.display_url);
       })
       .catch(function (error) {
         console.log(error);
       });
-  }
-  console.log(bookImageUrl)
+  };
+  console.log(bookImageUrl);
   return (
     <form className="p-5 bg-secondary" onSubmit={handleSubmit(onSubmit)}>
-
-      <input name="bookName" className="form-control m-1" type="text" placeholder="Book Name" ref={register({ required: true })} />
+      <input
+        name="bookName"
+        className="form-control m-1"
+        type="text"
+        placeholder="Book Name"
+        ref={register({ required: true })}
+      />
       {errors.exampleRequired && <span>This field is required</span>}
-      <input name="authorName" className="form-control m-1" type="text" placeholder="Author Name" ref={register({ required: true })} />
+      <input
+        name="authorName"
+        className="form-control m-1"
+        type="text"
+        placeholder="Author Name"
+        ref={register({ required: true })}
+      />
       {errors.exampleRequired && <span>This field is required</span>}
-      <input name="bookPrice" className="form-control m-1" type="text" placeholder="Book Price" ref={register({ required: true })} />
+      <input
+        name="bookPrice"
+        className="form-control m-1"
+        type="text"
+        placeholder="Book Price"
+        ref={register({ required: true })}
+      />
       {errors.exampleRequired && <span>This field is required</span>}
-      <input name="bookImage" onChange={handleBookImage}  className="form-control m-1 pb-4" type="file" ref={register({ required: true })} />
+      <input
+        name="bookImage"
+        onChange={handleBookImage}
+        className="form-control m-1 pb-4"
+        type="file"
+        ref={register({ required: true })}
+      />
       {errors.exampleRequired && <span>This field is required</span>}
-      
-
+      {
+        Linear && <LinearProgress></LinearProgress>
+      }
       <input type="submit" className="form-control m-1 btn-primary" />
     </form>
   );
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,29 +1,69 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { UserContext } from "../../App";
 const Header = () => {
-  const [loggedInUser, setLoggedInUser]= useContext(UserContext)
-  const handleLogout=()=>{
-      setLoggedInUser('')
-  }
+  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+  const handleLogout = () => {
+    setLoggedInUser("");
+  };
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
   return (
-    <div className="d-flex justify-content-between  pl-5 pr-5 bg-light font-weight-bold text-dark">
-      <div className="logo">
-        <h2 className="text-dark h2 font-weight-bold" >BOOK BAZAR</h2>
+    <nav class=" text-dark font-weight-bold navbar navbar-expand-lg navbar-light bg-light rounded">
+      <div className="logo mr-5 pr-5">
+        <h2 className="text-dark h2 font-weight-bold ml-md-3 pr-md-5 mr-md-5">BOOK BAZAR</h2>
       </div>
-      <div className="pt-2">
-        <NavLink className="mr-3 p-2 text-dark" to="/home">Home</NavLink>
-        <NavLink className="mr-3 p-2 text-dark" to="/orders">Orders</NavLink>
-        <NavLink className="mr-3 p-2 text-dark" to="/admin">Admin</NavLink>
-        <NavLink className="mr-3 p-2 text-dark" to="/deals">Deals</NavLink>
-        {
-          !loggedInUser.email ? <NavLink className="mr-3 p-2 text-dark rounded btn-outline-primary" to="/login">Login</NavLink> :<span onClick={handleLogout} className="mr-3 p-2 text-dark rounded btn-outline-primary" >logout</span>
-        }
-        
-        
+
+      <button
+        class="custom-toggler navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarsExample09"
+        aria-controls="navbarsExample09"
+        aria-expanded={!isNavCollapsed ? true : false}
+        aria-label="Toggle navigation"
+        onClick={handleNavCollapse}
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      {/* <div className="pt-2"> */}
+      <div
+        class={`${isNavCollapsed ? "collapse" : ""} navbar-collapse`}
+        id="navbarsExample09"
+        className="ml-md-5 pl-md-5"
+      >
+        <NavLink className="mr-3 p-2 text-dark" to="/home">
+          Home
+        </NavLink>
+        <NavLink className="mr-3 p-2 text-dark" to="/orders">
+          Orders
+        </NavLink>
+        <NavLink className="mr-3 p-2 text-dark" to="/admin">
+          Admin
+        </NavLink>
+        <NavLink className="mr-3 p-2 text-dark" to="/deals">
+          Deals
+        </NavLink>
+        {!loggedInUser.email ? (
+          <NavLink
+            className="mr-3 p-2 text-dark rounded btn-outline-primary"
+            to="/login"
+          >
+            Login
+          </NavLink>
+        ) : (
+          <span
+            onClick={handleLogout}
+            className="mr-3 p-2 text-dark rounded btn-outline-primary"
+          >
+            logout
+          </span>
+        )}
       </div>
-      
-    </div>
+      {/* </div> */}
+    </nav>
   );
 };
 
